b03383663d37404ddf8ec971c90b30487c38bedd
mahayukki <jonassouza200@gmail.com>
2025-05-12 20:41:39 +0000

    cartas

---
+++ cartassupertrunfo.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+        int main() {
+    // Carta 01
+    printf("Carta Cidade são paulo, carta 1 campinas"); {
+        "01";
+        "São Paulo";
+        "Campinas";
+        printf("populaçao = 1213792");
+        printf("área  = 795.7");
+        printf("pib   =64.5");
+        printf("pontos turisticos = 12");
+    }
+
+    // Carta 02
+    printf("Carta Cidade Salvador (carta 2 Bahia"); {
+        "02";
+        "Bahia";
+        "Salvador";
+        printf("populaçao = 2886698");
+        printf("área = 692.8");
+        printf("pib = 63.1");
+        printf("pontos turisticos = 15");
+    }
+
+    printf("=== Cartas de Cidades ===\n\n");
+    printf("exibirCarta (carta1");
+    printf("exibirCarta(carta2");
+
+}
